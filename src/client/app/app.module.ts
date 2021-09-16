10e4cd7c70e128c703b936d94c9f904f21fa25a0
Jonathan Hinton <hintonjonathan@gmail.com>
2021-09-16 14:15:08 -0500

    Scaffold UI

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParkListComponent } from './park-list/park-list.component';
+import { ParkComponent } from './park/park.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParkListComponent,
+    ParkComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/client/app/park-list/park-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-park-list',
+  templateUrl: './park-list.component.html',
+  styleUrls: ['./park-list.component.scss']
+})
+export class ParkListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
