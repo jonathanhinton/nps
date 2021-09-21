b8544522399a1d98a94a1ddb6d98ebbead40c0f2
Jonathan Hinton <hintonjonathan@gmail.com>
2021-09-21 14:38:56 -0500

    add routing for park detail page

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ParkListComponent } from './park-list/park-list.component';
+import { ParkDetailComponent } from './park-detail/park-detail.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'parks',
     component: ParkListComponent
+  },
+  {
+    path: 'park/:id', 
+    component:ParkDetailComponent
   }
 ];
 
