e9039e0aa60540911c4972c8eef2e2627e1ea285
Jonathan Hinton <hintonjonathan@gmail.com>
2021-09-21 14:40:14 -0500

    re-factor getParks and add getPark by id method

--- src/client/app/shared/api.service.ts
+++ src/client/app/shared/api.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, catchError, retry } from 'rxjs/operators'
-import { Observable, throwError } from 'rxjs';
+import { map, catchError, retry, tap } from 'rxjs/operators'
+import { Observable, of, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Park } from './park.model';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private baseUrl = environment.apiUrl;
+  private parksUrl = environment.apiUrl + '/parks';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient, 
+    private messageService: MessageService) { }
 
   httpHeader = {
     headers: new HttpHeaders({
@@ -22,23 +25,49 @@ export class ApiService {
     })
   }
 
-  getParks(): Observable<Park> {
-    return this.http.get<Park>(this.baseUrl + '/parks');
-      // .pipe(
-      //   retry(1),
-      //   catchError(this.httpError)
-      // )
+  // GET all parks from server
+  getParks(): Observable<Park[]> {
+    return this.http.get<Park[]>(this.parksUrl)
+       .pipe(
+         catchError(this.handleError<Park[]>('getParks', []))
+       );
   }
 
-  // httpError(error) {
-  //   let msg = '';
-  //   if(error.error instanceof ErrorEvent) {
-  //     msg = error.error.message;
-  //   } else {
-  //     msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
-  //   }
-  //   console.log(msg);
-  //   return throwError(msg);
-  // }
+  // GET Park by id. Return 'undefined' if park id not found
+  getParkNo404<Data>(id: string): Observable<Park> {
+    const url = `${this.parksUrl}/?id=${id}`;
+    return this.http.get<Park[]>(url)
+      .pipe(
+        map(parks => parks[0]),
+        tap(p => {
+          const outcome = p ? `fetched` : `did not find`;
+          this.log(`${outcome} park id =${id}`);
+        }),
+        catchError(this.handleError<Park>(`getPark id=${id}`))
+      );
+  }
 
+  // GET park by id. 404 if id not found
+  getPark(id: string): Observable<Park> {
+    const url = `${this.parksUrl}/${id}`;
+    return this.http.get<Park>(url)
+      .pipe(
+        tap(_ => this.log(`fetched park id=${id}`)),
+        catchError(this.handleError<Park>(`getPark id=${id}`))
+      );
+  }
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error:any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
+
+  private log(message: string) {
+    this.messageService.add(`ApiService: ${message}`);
+  }
+  
 }
