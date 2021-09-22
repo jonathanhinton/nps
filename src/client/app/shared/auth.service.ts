350dd7f05cc95b971393c28a34ccd5d8846c3804
Jonathan Hinton <hintonjonathan@gmail.com>
2021-09-22 10:42:55 -0500

    add methods for handling user authentication

--- src/client/app/shared/auth.service.ts
+++ src/client/app/shared/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  storageKey: string = "park-explorer-jwt"
+
+  constructor(private router: Router) { }
+
+  setToken(token: string) {
+    localStorage.setItem(this.storageKey, token);
+  }
+
+  getToken() {
+    return localStorage.getItem(this.storageKey);
+  }
+
+  isLoggedIn() {
+    return this.getToken() !== null;
+  }
+
+  logout() {
+    localStorage.removeItem(this.storageKey);
+    this.router.navigate(['/login']);
+  }
 }
