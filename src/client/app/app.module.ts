933e08760379e156fd93ad698198c7326e69672d
Jonathan Hinton <hintonjonathan@gmail.com>
2021-09-23 14:17:52 -0500

    add menu component

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -14,6 +14,7 @@ import { FilterPipe } from './shared/filter.pipe';
 import { ParkDetailComponent } from './park-detail/park-detail.component';
 import { LoginComponent } from './login/login.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CreateAccountComponent } from './create-account/create-account.componen
     FilterPipe,
     ParkDetailComponent,
     LoginComponent,
-    CreateAccountComponent
+    CreateAccountComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
